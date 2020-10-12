0699830c1ef6ac1cb8df62a622b69d2471d7687a
ljgalaty <laurel.galaty@gmail.com>
2020-10-11 23:31:16 -0400

    Working Game, just need to write the trivia questions

--- trivia-game.js
+++ trivia-game.js
@@ -1,114 +1,117 @@
-let  readlineSync  =  require('readline-sync');
+const readlineSync  =  require('readline-sync');
 
-const maxTries = 5;
+const maxTries = 10;
 let currentTries = 0;
+const winScore = 7;
 let scoreCounter = 0;
+readlineSync.setDefaultOptions({limit: ["a", "b", "c", "d"]});
 
-let userInput = prompt.question(
-    `Welcome to the trivia game!/n 
-    To win the game, get 10 points in ${maxTries} tries or less. Otherwise, you lose./n 
-    Easy questions are worth 1 pt, intermediate questions are worth 2 points, and hard questions are worth 3 points.`, )
+let userInput = readlineSync.question(
+    `Welcome to the trivia game! 
+    To win the game, get 7 points in ${maxTries} tries or less. Otherwise, you lose. 
+    Type a and hit enter when you're ready!\n`);
 
 const playGame = userInput => {
-    for (let currentTries = 0; tries < maxTries; tries++) {
+    for (let currentTries = 0; currentTries < maxTries; currentTries++) {
         if (scoreCounter >= 10) {
             console.log(`You win!`);
         } else if (currentTries === maxTries) {
             console.log(`You ran out of tries.`);
         } else {
-            console.log(`Pick your next question. Would you like it to be a) Easy(+1 pt), b) Medium(+2 pts), or c) Hard(+3 pts)? Please enter a, b, or c when you've made your choice.`)
+            console.log(`You have ${maxTries - currentTries} tries left to get ${winScore - scoreCounter} points.`);
+            console.log(questionBank[currentTries].question);
+            userInput = readlineSync.question(questionBank[currentTries].options);
+                if (userInput === questionBank[currentTries].correctAnswer) {
+                    scoreCounter++;
+                    console.log("That's correct! You've earned a point!");
+                    userInput = readlineSync.question("Press a and enter when you're ready to proceed.");
+                } else {
+                    console.log("That's incorrect. You have earned no points.");
+                    userInput = readlineSync.question("Press a and enter when you're ready to proceed.");
+                }
         }
     }
 };
 
-
-
-
-
-// switch() {
-//     case "a":
-//         console.log("Question from easy question bank")
-//         break;
-//     case "b":
-//         console.log("question from medium question bank")
-//         break;
-//     case c:
-//         console.log("question from hard question bank")
-//         break;
-//     default:
-//         console.log(`please choose one of the listed options`)
-// }
-
-
-// let questionBank {
-//     "Easy": {
-//         "question1": {
-//             "question": "This is the question",
-//             "options": "a) b) c)",
-//             "correctAnswer": "b)"
-//         },
-//         "question2": {
-//             "question": "This is the question",
-//             "options": "a) b) c)",
-//             "correctAnswer": "b)"
-//         },
-//         "question3": {
-//             "question": "This is the question",
-//             options: "a) b) c)",
-//             correctAnswer: "b)"
-//         },
-//         question4: {
-//             question: "This is the question",
-//             options: "a) b) c)",
-//             correctAnswer: "b)"
-//         }
+let questionBank = [
+    {
+        question: "Does this work?",
+        options: "a) yes b) no",
+        correctAnswer: "a"
+    },
+    {
+        question: "Does this work?",
+        options: "a) yes b) no",
+        correctAnswer: "a"
+    }
+];
+// let questionBank = [
+//     question1: {
+//         question: "This is the question",
+//         options: "a) b) c)",
+//         correctAnswer: "b"
 //     },
-//     "Medium": {
-//         question1: {
-//             question: "This is the question",
-//             options: "a) b) c)",
-//             correctAnswer: "b)"
-//         },
-//         question2: {
-//             question: "This is the question",
-//             options: "a) b) c)",
-//             correctAnswer: "b)"
-//         },
-//         question3: {
-//             question: "This is the question",
-//             options: "a) b) c)",
-//             correctAnswer: "b)"
-//         },
-//         question4: {
-//             question: "This is the question",
-//             options: "a) b) c)",
-//             correctAnswer: "b)"
-//         }
+//     question2: {
+//         question: "This is the question",
+//         options: "a) b) c)",
+//         correctAnswer: "b)"
 //     },
-//     "Hard": {
-//         question1: {
-//             question: "This is the question",
-//             options: "a) b) c)",
-//             correctAnswer: "b)"
-//         },
-//         question2: {
-//             question: "This is the question",
-//             options: "a) b) c)",
-//             correctAnswer: "b)"
-//         },
-//         question3: {
-//             question: "This is the question",
-//             options: "a) b) c)",
-//             correctAnswer: "b)"
-//         },
-//         question4: {
-//             question: "This is the question",
-//             options: "a) b) c)",
-//             correctAnswer: "b)"
-//         }
+//     question3: {
+//         question: "This is the question",
+//         options: "a) b) c)",
+//         correctAnswer: "b)"
 //     },
-// }
+//     question4: {
+//         question: "This is the question",
+//         options: "a) b) c)",
+//         correctAnswer: "b)"
+//     },
+//     question5: {
+//         question: "This is the question",
+//         options: "a) b) c)",
+//         correctAnswer: "b)"
+//     },
+//     question6: {
+//         question: "This is the question",
+//         options: "a) b) c)",
+//         correctAnswer: "b"
+//     },
+//     question7: {
+//         question: "This is the question",
+//         options: "a) b) c)",
+//         correctAnswer: "b)"
+//     },
+//     question8: {
+//         question: "This is the question",
+//         options: "a) b) c)",
+//         correctAnswer: "b)"
+//     },
+//     question9: {
+//         question: "This is the question",
+//         options: "a) b) c)",
+//         correctAnswer: "b)"
+//     },
+//     question10: {
+//         question: "This is the question",
+//         options: "a) b) c)",
+//         correctAnswer: "b)"
+//     },
+//     question11: {
+//         question: "This is the question",
+//         options: "a) b) c)",
+//         correctAnswer: "b)"
+//     },
+//     question12: {
+//         question: "This is the question",
+//         options: "a) b) c)",
+//         correctAnswer: "b)"
+//     }
+// ];
+
+playGame();
    
+
+
 
 
 
