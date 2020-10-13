c0fb37eb0958c7190474a651653ab4dab7bd9b08
ljgalaty <laurel.galaty@gmail.com>
2020-10-12 23:01:40 -0400

    This should be working

--- trivia-game.js
+++ trivia-game.js
@@ -1,19 +1,19 @@
 const readlineSync  =  require('readline-sync');
 
-const maxTries = 10;
+const maxTries = 8;
 let currentTries = 0;
-const winScore = 7;
+const winScore = 5;
 let scoreCounter = 0;
 readlineSync.setDefaultOptions({limit: ["a", "b", "c", "d"]});
 
 let userInput = readlineSync.question(
     `Welcome to the trivia game! 
-    To win the game, get 7 points in ${maxTries} tries or less. Otherwise, you lose. 
+    To win the game, get 5 points in ${maxTries} tries or less. Otherwise, you lose. 
     Type a and hit enter when you're ready!\n`);
 
 const playGame = userInput => {
     for (let currentTries = 0; currentTries < maxTries; currentTries++) {
-        if (scoreCounter >= 10) {
+        if (scoreCounter >=5) {
             console.log(`You win!`);
         } else if (currentTries === maxTries) {
             console.log(`You ran out of tries.`);
@@ -50,54 +50,45 @@ let questionBank = [
     {
         question: "A young male is brought to the emergency department by family for 24 hours of unresponsiveness. Family says he has not eaten or drunk anything during this time.He is alert and awake, makes eye contact, but he does not talk. He does not make purposeful movements, but when you lift his arm, he holds it just where you left it. What would you do?",
         options: "a) Nothing; he's faking. b) Call a neurologist; he's having a stroke! c) Treat his anxiety. This is psychiatric.",
-        correctAnswer: "c"
+        correctAnswer: "c",
         explanation: "This patient is suffering from a psychiatric illness known as catatonia, typically induced by severe psychological trauma. Many of these patients exhibit a quality known as 'waxiness,' in which they hold the position they're placed in. Unless they're all Googling this, pretty hard to fake. Strong anti-anxiety medications typically resolve the symptoms."
     },
     {
-        question: "Does this work?",
-        options: "a) yes b) no",
-        correctAnswer: "a"
+        question: "What's my name?",
+        options: "a) Bob b) Laurel c) Christopher",
+        correctAnswer: "b",
+        explanation: "It's true, my name is Laurel"
     },
     {
-        question: "Does this work?",
-        options: "a) yes b) no",
-        correctAnswer: "a"
+        question: "What's my middle name?",
+        options: "a) Marie b) John c) Jolie d) Bilbo",
+        correctAnswer: "c",
+        explanation: "It would be cool if my middle name was Bilbo but it's not."
     },
     {
-        question: "Does this work?",
+        question: "Does this game work?",
         options: "a) yes b) no",
-        correctAnswer: "a"
+        correctAnswer: "a",
+        explanation: "I hope you said Yes."
     },
     {
-        question: "Does this work?",
-        options: "a) yes b) no",
-        correctAnswer: "a"
+        question: "What was the name of my Great Dane when I was a kid",
+        options: "a) Winston b) Dane c) Rueben d) Maya",
+        correctAnswer: "d",
+        explanation: "Maya was the best dog in the world"
     },
     {
-        question: "Does this work?",
+        question: "Did you notice that the questions radically changed in the middle of the game?",
         options: "a) yes b) no",
-        correctAnswer: "a"
+        correctAnswer: "a",
+        explanation: "Sorry about that, this was going to be a medical trivia game, but things change and that's just life."
     },
     {
-        question: "Does this work?",
-        options: "a) yes b) no",
-        correctAnswer: "a"
-    },
-    {
-        question: "Does this work?",
-        options: "a) yes b) no",
-        correctAnswer: "a"
-    },
-    {
-        question: "Does this work?",
-        options: "a) yes b) no",
-        correctAnswer: "a"
-    },
-    {
-        question: "Does this work?",
-        options: "a) yes b) no",
-        correctAnswer: "a"
-    },
+        question: "How many times have I been to the ABBA museum in Sweden?",
+        options: "a) never b) once c) twice d) thrice",
+        correctAnswer: "d",
+        explanation: "I have truly been there 3 times and I plan to go several more times. Long live Eurovision."
+    }
 ];
 
 playGame();
